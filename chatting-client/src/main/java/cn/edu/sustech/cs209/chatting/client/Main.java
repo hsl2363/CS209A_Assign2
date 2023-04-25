ca2caed919886ec43d09a310a946aa3674da0752
hsl2363 <hshaolinv@sina.com>
2023-04-25 22:11:47 +0800

    User operation checked

--- chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
+++ chatting-client/src/main/java/cn/edu/sustech/cs209/chatting/client/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     private Controller controller;
 
-    public void main(String[] args) {
+    public static void main(String[] args) {
         launch();
     }
 
